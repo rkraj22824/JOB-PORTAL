35b1c0cd3323d1b70487bde9265bd6806ba986d3
rkraj22824 <rkumar992001@gmail.com>
2024-08-25 19:02:09 +0530

    changes of the day

--- backend/controller/job.controller.js
+++ backend/controller/job.controller.js
@@ -54,10 +54,12 @@ export const getAllJobs = async (req, res) => {
                 success: false
             })
         };
+    
         return res.status(200).json({
             jobs,
             success: true
         })
+        
     } catch (error) {
         console.log(error);
     }
@@ -67,17 +69,22 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
     try {
         const jobId = req.params.id;
-        const job = await Job.findById(jobId);
+        const job = await Job.findById(jobId).populate({
+            path:"applications"
+        });
         if (!job) {
             return res.status(404).json({
                 message: "Jobs not found.",
                 success: false
             })
         };
+        console.log(res);
         return res.status(200).json({
             job,
             success: true
         });
+
+       
     } catch (error) {
         console.log(error);
     }
--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 
 export const register = async (req, res) => {
@@ -13,9 +15,9 @@ export const register = async (req, res) => {
                 success: false
             });
         };
-        // const file = req.file;
-        // const fileUri = getDataUri(file);
-        // const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const file = req.file;
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
 
         const user = await User.findOne({ email });
         if (user) {
@@ -32,9 +34,9 @@ export const register = async (req, res) => {
             phoneNumber,
             password: hashedPassword,
             role,
-            // profile:{
-            //     profilePhoto:cloudResponse.secure_url,
-            // }
+            profile:{
+                profilePhoto:cloudResponse.secure_url,
+            }
         });
 
         return res.status(201).json({
@@ -91,9 +93,6 @@ export const login = async (req, res) => {
             role: user.role,
             profile: user.profile
         }
-
-       
-
         return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
             message: `Welcome back ${user.fullname}`,
             user,
@@ -121,12 +120,14 @@ export const logout = async (req, res) => {
 export const updateProfile = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, bio, skills } = req.body;
+        
+        
         const file = req.file;
 
 
         // // cloudinary ayega idhar
-        // const fileUri = getDataUri(file);
-        // const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
 
  
 
@@ -152,10 +153,10 @@ export const updateProfile = async (req, res) => {
         if(skills) user.profile.skills = skillsArray
 
         // resume comes later here...
-        // if(cloudResponse){
-        //     user.profile.resume = cloudResponse.secure_url // save the cloudinary url
-        //     user.profile.resumeOriginalName = file.originalname // Save the original file name
-        // }
+        if(cloudResponse){
+            user.profile.resume = cloudResponse.secure_url // save the cloudinary url
+            user.profile.resumeOriginalName = file.originalname // Save the original file name
+        }
 
 
         await user.save();

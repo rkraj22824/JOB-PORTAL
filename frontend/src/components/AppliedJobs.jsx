72058914cbee7888325f206df8f613d59bcb30a8
rkraj22824 <rkumar992001@gmail.com>
2024-08-28 19:36:13 +0530

    admin

--- frontend/src/components/AppliedJobs.jsx
+++ frontend/src/components/AppliedJobs.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table';
+import { Badge } from './ui/badge';
+import { useSelector } from 'react-redux';
 
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
-import { Badge } from './ui/badge'
-import { useSelector } from 'react-redux'
+const AppliedJob = () => {
+    const { allAppliedJobs } = useSelector(store => store.job);
 
-const AppliedJobs = () => {
-    const {allappliedJobs } = useSelector(store =>store.job)
-    console.log(allappliedJobs);
-  return (
-    <div>
+    return (
+        <div>
             <Table>
                 <TableCaption>A list of your applied jobs</TableCaption>
                 <TableHeader>
@@ -20,20 +19,39 @@ const AppliedJobs = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                {
-                        allappliedJobs?.length <= 0 ? <span>You haven't applied any job yet.</span> : allappliedJobs?.map((appliedJob) => (
-                            <TableRow key={appliedJob._id}>
-                                <TableCell>{appliedJob?.createdAt?.split("T")[0]}</TableCell>
-                                <TableCell>{appliedJob.job?.title}</TableCell>
-                                <TableCell>{appliedJob.job?.company?.name}</TableCell>
-                                <TableCell className="text-right"><Badge className={`${appliedJob?.status === "rejected" ? 'bg-red-400' : appliedJob.status === "pending" ? 'bg-gray-400' : 'bg-green-400'}`}>{appliedJob.status.toUpperCase()}</Badge></TableCell>
-                            </TableRow>
-                        ))
+                    {
+                        allAppliedJobs.length <= 0 ? (
+                            <span>You haven't applied to any job yet.</span>
+                        ) : (
+                            allAppliedJobs.map((appliedJob) => {
+                                // Check if appliedJob and its properties are valid
+                                if (!appliedJob || !appliedJob.job) {
+                                    return (
+                                        <TableRow key={Math.random()}>
+                                            <TableCell colSpan="4">Invalid job data</TableCell>
+                                        </TableRow>
+                                    );
+                                }
+
+                                return (
+                                    <TableRow key={appliedJob._id}>
+                                        {/* <TableCell>{appliedJob.createdAt ? appliedJob.createdAt.split("T")[0] : 'N/A'}</TableCell> */}
+                                        <TableCell>{appliedJob.job.title || 'No Title'}</TableCell>
+                                        <TableCell>{appliedJob.job.company?.name || 'No Company'}</TableCell>
+                                        <TableCell className="text-right">
+                                            <Badge className={`${appliedJob.status === "rejected" ? 'bg-red-400' : appliedJob.status === 'pending' ? 'bg-gray-400' : 'bg-green-400'}`}>
+                                                {appliedJob.status.toUpperCase()}
+                                            </Badge>
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })
+                        )
                     }
                 </TableBody>
             </Table>
         </div>
-  )
-}
+    );
+};
 
-export default AppliedJobs
+export default AppliedJob;
